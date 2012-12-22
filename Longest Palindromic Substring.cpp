8b43d68f1f3a502d470c1783c3c5998dde7dfb61
charleyzhuyi <charleyzhuyi@hotmail.com>
2012-12-22 04:38:25 -0800

    Update Longest Palindromic Substring.cpp

--- Longest Palindromic Substring.cpp
+++ Longest Palindromic Substring.cpp
@@ -40,4 +40,50 @@ public:
         
         return resultStr;
     }
+    
+    //dp solution, still O(n2) computation time. and O(n2)space 
+     string longestPalindrome(string s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int n = s.size();
+        bool check[1000][1000] = {false};
+        int maxLen = 0;
+        int begin = 0;
+        string result;
+        //base case
+        for (int i=0;i<n;i++) {
+            check[i][i]=true;
+            maxLen = 1;
+            result = s.substr(i,1);
+        }
+        for (int i=0;i<n-1;i++) {
+            if (check[i][i] && s[i]==s[i+1]){
+                
+                check[i][i+1]=true;
+                 maxLen = 2;
+                begin = i;
+            }
+        }
+        
+        for (int len=3;len<=n;len++) {
+            
+            for (int i=0;i<n-len+1;i++) {
+                
+                int j = i+len-1;
+               if (check[i+1][j-1] && s[i] == s[j]) {
+                   
+                   check[i][j] = true;
+                   maxLen = len;
+                   begin = i;
+               }
+                
+                
+            }
+            
+            
+        }
+        
+        return s.substr(begin,maxLen);
+        
+    }
 };
