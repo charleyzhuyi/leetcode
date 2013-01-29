f5165dfcdcc38f5555de5e46ecea5a63568d619f
charleyzhuyi <charleyzhuyi@hotmail.com>
2013-01-29 04:30:12 -0800

    Update Generate Parentheses.cpp

--- Generate Parentheses.cpp
+++ Generate Parentheses.cpp
@@ -1,51 +1,29 @@
 class Solution {
 public:
-
     vector<string> result;
-    
-    void recursiveParenthesis(int numLeft, int numRight,string str) {
-        
-        if (numLeft == 0 && numRight == 0) return;
-        if (numLeft == 0 && numRight > 0) {
-            
-            string newStr (str);
-            for (int i=0;i<numRight;i++) {
-                newStr = newStr + ')';
-            }
-            
-            result.push_back(newStr);
-            
-            return;
-        }
-        if (numLeft > 0 && numRight == 0) {
-            cout << "ERROR";
-            return;
-        }
-        
-        
-        recursiveParenthesis(numLeft-1,numRight,str + '(');
-        
-        if (numRight > numLeft) 
-            recursiveParenthesis(numLeft,numRight-1, str + ')');
-        
-        
-        
-    }
-
     vector<string> generateParenthesis(int n) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-       
-         
-        if ( n < 1) 
-            return result;
-        
-       
-        string mystring;
-        
         result.clear();
-        recursiveParenthesis(n,n,mystring);
-        
+        mygen(n,n,"");
         return result;
+    }
+    
+    void mygen(int left,int right,string str){
+        if (left > right) return;
+        if (left == 0 ) {
+            while (right>0) {
+                str += ')';
+                right--;
+            }
+            result.push_back(str);
+            return;
+        }
+        
+        if (right>left) {
+            mygen(left,right-1,str+')');
+        }
+        
+        mygen(left-1,right,str+'(');
     }
 };
