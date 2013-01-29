9c2cfc4682c4ca8dc2ae8c9ba257812ab6fd2e4d
charleyzhuyi <charleyzhuyi@hotmail.com>
2013-01-29 06:02:06 -0800

    Update Implement strStr().cpp

--- Implement strStr().cpp
+++ Implement strStr().cpp
@@ -3,57 +3,19 @@ public:
     char *strStr(char *haystack, char *needle) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        
-        
         if (*haystack == '\0' &&  *needle == '\0') return "";
         if (*haystack != '\0' &&  *needle == '\0') return haystack;
         
-        char *p ;
-        char *save = NULL;
-        bool matched = false;
-        
         int len1 = strlen(haystack);
         int len2 = strlen(needle);
-        
-        for (int i=0;i<len1-len2+1;i++) {
-            
-            p = &haystack[i];
-            
-            char *n = needle;
-            
-            if ( *p == *n) {
-                
-                save = p;
-                
-                
-                matched = true;
-                
-                while ( *n != '\0') {
-                    
-                    if (*p == * n) {
-                        
-                        p++;
-                        n++;
-                    }else {
-                        matched = false;
-                        break;
-                    }
-                    
-                }
-                
-                if (matched) {
-                    break;
-                } else {
-                    p = save;
-                }
-                
+        for (int i=0;i<len1-len2+1;i++){
+            int p = i;
+            for (int j=0;j<len2;j++){
+                if (haystack[p] != needle[j]) break;
+                if (j == len2-1) return haystack+i;
+                p++;
             }
-            p++;
-            
         }
-        if (matched)
-            return save;
-        else
-            return NULL;
+        return NULL;
     }
 };
