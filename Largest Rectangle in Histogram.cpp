cc425508117fe6e1b1479db4e3002ec2ed81b949
charleyzhuyi <charleyzhuyi@hotmail.com>
2013-01-31 00:52:30 -0800

    Update Largest Rectangle in Histogram.cpp

--- Largest Rectangle in Histogram.cpp
+++ Largest Rectangle in Histogram.cpp
@@ -1,37 +1,26 @@
-//Time is O(n2), not a best solution, by using two stack, we can get a O(n) 
 class Solution {
 public:
-
     int largestRectangleArea(vector<int> &height) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        int maxArea  = 0 ;
-        int width[height.size()];
-        
-        for (int i=0;i<height.size();i++) {
-            width[i] = 1; 
-            //go left as far as possible
-            int j = i;
-            while ( j-1>=0 && height[j-1]>=height[i]) {  
-                width[i]++;
-                j--;
-            }
-            //go right as far as poosible
-            int k = i;
-            while (k+1 <height.size() && height[k+1] >= height[i]) {
-                width[i]++;
-                k++;
-            }
+        int n = height.size();
+        int width[n];
+        stack<int> st1,st2;
+        for (int i=0;i<n;i++){
+            while (!st1.empty() && height[st1.top()] >= height[i]) st1.pop();
+            width[i] = st1.empty()?i+1:i-st1.top();
+            st1.push(i);
         }
-        
-        for (int i=0;i<height.size();i++) { 
-                int tmpArea = width[i]*height[i];
-                if (tmpArea > maxArea)
-                    maxArea = tmpArea;  
-            }
-            
+        for (int j=n-1;j>=0;j--){
+            while (!st2.empty() && height[st2.top()] >= height[j]) st2.pop();
+            width[j] += st2.empty()?n-j:st2.top()-j;
+            st2.push(j);
+        }
+        int maxArea = 0;
+        for (int k=0;k<n;k++){
+            int area = height[k]*(width[k]-1);
+            maxArea = max(maxArea,area);
+        }
         return maxArea;
-        
-        
     }
 };
