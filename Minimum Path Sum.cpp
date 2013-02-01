f729fab870f2aeacee1aedc10ac821f8b1d8643b
charleyzhuyi <charleyzhuyi@hotmail.com>
2013-02-01 06:25:53 -0800

    Update Minimum Path Sum.cpp

--- Minimum Path Sum.cpp
+++ Minimum Path Sum.cpp
@@ -26,3 +26,24 @@ public:
         return f[m-1][n-1];
     }
 };
+
+/* Another way of doing it */
+class Solution {
+public:
+    int minPathSum(vector<vector<int> > &grid) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int m = grid.size();
+        int n = grid[0].size();
+        int w[m][n];
+        //Base Case
+        w[m-1][n-1] = grid[m-1][n-1];
+        for (int i=m-2;i>=0;i--) w[i][n-1] = w[i+1][n-1]+grid[i][n-1];
+        for (int j=n-2;j>=0;j--) w[m-1][j] = w[m-1][j+1]+grid[m-1][j];
+        for (int i=m-2;i>=0;i--)
+            for (int j=n-2;j>=0;j--)
+                w[i][j] = min(w[i+1][j],w[i][j+1])+grid[i][j];
+           
+        return w[0][0];
+    }
+};
