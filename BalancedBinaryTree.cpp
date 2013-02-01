4ab14101e791912ff51de090e6404793badce616
charleyzhuyi <charleyzhuyi@hotmail.com>
2013-02-01 05:53:58 -0800

    Update BalancedBinaryTree.cpp

--- BalancedBinaryTree.cpp
+++ BalancedBinaryTree.cpp
@@ -7,17 +7,37 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    bool isBalanced(TreeNode *node) {
+    bool isBalanced(TreeNode *root) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if (node == NULL) return true;  
-        return abs(maxDepth(node->left)-maxDepth(node->right))<=1 && isBalanced(node->left) && isBalanced(node->right);
- 
+        int h=0;
+        return checkBalanced(root,h);
     }
-    int maxDepth (TreeNode *node) {
-        if (node == NULL) return true;
-        return max(maxDepth(node->left),maxDepth(node->right))+1;
+    
+    bool checkBalanced(TreeNode *node,int &height){
+        if (node == NULL){
+            height = 0;
+            return true;
+        }
+        int lh = 0;
+        int rh = 0;
+        if (checkBalanced(node->left,lh) == false) return false;
+        if (checkBalanced(node->right,rh) == false) return false;
+        
+        int diff = abs(lh-rh);
+        height = max(lh,rh)+1;
+        if (diff <= 1) return true;
+        else return false;
     }
 };
