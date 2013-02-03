dc8da657a7e519ea7c22f643c50edfba75ba5aee
charleyzhuyi <charleyzhuyi@hotmail.com>
2013-02-03 03:21:26 -0800

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -38,3 +38,32 @@ public:
         return result;
     }
 };
+
+/* Another solution */
+class Solution {
+public:
+    vector<vector<int> > result;
+    vector<vector<int> > permute(vector<int> &num) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        result.clear();
+        permute(num,0);
+        return result;
+    }
+    void permute(vector<int> num,int locPos){
+        if (locPos == num.size()-1){
+            result.push_back(num);
+            return;
+        }
+        for (int i=locPos;i<num.size();i++){
+            swapVector(num,locPos,i);
+            permute(num,locPos+1);
+            swapVector(num,locPos,i);
+        }
+    }
+    void swapVector(vector<int> &num,int a, int b){
+        int tmp = num[a];
+        num[a] = num[b];
+        num[b] = tmp;
+    }
+};
