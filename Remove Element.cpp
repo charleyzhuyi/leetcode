3137f77a80eb8a126b4da2ac196a514bc95a97e6
charleyzhuyi <charleyzhuyi@hotmail.com>
2013-02-04 01:24:50 -0800

    Create Remove Element.cpp

---
+++ Remove Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (n==0) return 0;
+        int i=0;
+        for (int j=0;j<n;j++){
+            if (A[j] != elem){
+                A[i] = A[j];
+                i++;
+            }
+        }
+        return i;
+    }
+};
