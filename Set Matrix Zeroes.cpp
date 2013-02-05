3ea0663014483d494b606e1c76d34ccd8a9fded0
charleyzhuyi <charleyzhuyi@hotmail.com>
2013-02-04 23:38:31 -0800

    Create Set Matrix Zeroes.cpp

---
+++ Set Matrix Zeroes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int> > &matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int firstrow = 0;
+        int firstcol = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < m; ++j)
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    if (i == 0)
+                        firstrow = 1;
+                    if (j == 0)
+                        firstcol = 1;
+                }
+        for (int i = 1; i < n; ++i)
+            if (matrix[i][0] == 0)
+                for (int j = 0; j < m; ++j)
+                    matrix[i][j] = 0;
+        for (int j = 1; j < m; ++j)
+            if (matrix[0][j] == 0)
+                for (int i = 0; i < n; ++i)
+                    matrix[i][j] = 0;
+        if (firstrow)
+            for (int j = 0; j < m; ++j)
+                matrix[0][j] = 0;
+        if (firstcol)
+            for (int i = 0; i < n; ++i)
+                matrix[i][0] = 0;
+    }
+};
